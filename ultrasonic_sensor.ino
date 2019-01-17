4d47c91cf1695a5c489897d02734f3f6a4ccd961
jfro1978 <39474907+jfro1978@users.noreply.github.com>
2019-01-17 14:27:20 +1100

    Update ultrasonic_sensor.ino

--- ultrasonic_sensor.ino
+++ ultrasonic_sensor.ino
@@ -2,14 +2,14 @@
 
 int trig = 7;
 int echo = 8;
-unsigned long pulse_duration = 0
-int distance 
+unsigned long pulse_duration = 0;
+int distance;
 int loop_time = 1000;
 
 void setup() 
 {
   pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT)  
+  pinMode(echo, INPUT);  
 
   //Ensure trig pin is set to low to begin with
   digitalWrite(trig,0);
@@ -23,9 +23,9 @@ void loop()
   delayMicroseconds(10);
   digitalWrite(trig,0);
   
-  duration = pulseIn(echo, HIGH) * 1000000; // units of seconds
+  pulse_duration = pulseIn(echo, HIGH) * 1000000; // units of seconds
 
-  distance = (343*duration)/2*100;
+  distance = (343*pulse_duration)/2*100;
   
   Serial.print("The distance is: ");
   Serial.println(distance);
